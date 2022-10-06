81f74a52bb5a943cf95d5d211f886ea03d5b0ad3
Sean McGillis <slcidevice@yahoo.com>
2022-10-05 23:58:14 -0600

    Modified the getNotes function so I could programmatically add the new book property to all the notes stored on firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useState } from "react";
-import Header from './components/Header.js';
+import Header from "./components/Header.js";
 
 export default function App() {
   const NotesAPI = "https://my-notes-64d6a.firebaseio.com";
@@ -16,18 +16,41 @@ export default function App() {
       throw new Error(data.message || "Could not fetch quotes.");
     }
 
-    // console.log("data", data); //@DEBUG
-
     const transformedNotes = [];
 
+    // console.log('data', data); //@DEBUG
+
+    const noteObj = {};
+
     for (const key in data) {
-      const noteObj = {
-        id: key,
-        ...data[key],
-      };
-      transformedNotes.push(noteObj);
+      if (
+        data[key].page === "git" ||
+        data[key].page === "angularfire" ||
+        data[key].page === "misc" ||
+        data[key].page === "ionic" ||
+        data[key].page === "shortcuts" ||
+        data[key].page === "reactnative" ||
+        data[key].page === "reactjs" ||
+        data[key].page === "docker" ||
+        data[key].page === "vscode" ||
+        data[key].page === "simulator" ||
+        data[key].page === "interview"
+      ) {
+        noteObj[[key]] = { book: "developer", ...data[key] };
+      }
+      if (
+        data[key].page === "3dprinting" ||
+        data[key].page === "raspberrypi" ||
+        data[key].page === "arduino"
+      ) {
+        noteObj[[key]] = { book: "hobby", ...data[key] };
+      }
+      if (data[key].page === "appletv" || data[key].page === "updates") {
+        noteObj[[key]] = { book: "misc", ...data[key] };
+      }
     }
-    console.log(transformedNotes);
+    const myArr = [noteObj];
+    console.log(JSON.stringify(myArr)); //@DEBUG
 
     setNotes(transformedNotes);
   }
@@ -38,7 +61,7 @@ export default function App() {
   //   }
   //   if (pages.length < 1 && notes.length < 1) {
   //     return <p>Push the Fetch notes button</p>;
-  //   } 
+  //   }
   //   if (pages.length > 0) {
   //     return pages.map((page, index) => <p key={index}>{page}</p>)
   //   }
@@ -47,7 +70,7 @@ export default function App() {
   function getPages(myArr) {
     const pageArr = [];
     for (const key in myArr) {
-      const page = myArr[key].page
+      const page = myArr[key].page;
       if (!pageArr.includes(page)) {
         pageArr.push(page);
       }
@@ -56,12 +79,10 @@ export default function App() {
     setPages(pageArr);
   }
 
-  
-
   function executeSearch(myArr, keyWord) {
     const keyWordObjectsArr = [];
     for (const key in myArr) {
-      let myWord = myArr[key]
+      let myWord = myArr[key];
       if (myWord.content.toLowerCase().includes(keyWord)) {
         keyWordObjectsArr.push(myWord.content);
       }
@@ -75,8 +96,15 @@ export default function App() {
       <header className="App-header">
         {/* <ShowPages /> */}
         <button onClick={() => getNotes()}>Fetch notes</button>
-        <button disabled={notes.length < 1} onClick={() => getPages(notes)}>Show pages</button>
-        <button disabled={notes.length < 1} onClick={() => executeSearch(notes, 'install')}>Find install</button>
+        <button disabled={notes.length < 1} onClick={() => getPages(notes)}>
+          Show pages
+        </button>
+        <button
+          disabled={notes.length < 1}
+          onClick={() => executeSearch(notes, "install")}
+        >
+          Find install
+        </button>
         <div className="test">test</div>
       </header>
     </div>
