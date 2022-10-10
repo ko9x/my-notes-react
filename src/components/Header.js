d63884d7b780874671a9ddf1a54dbc63599995b7
Sean McGillis <slcidevice@yahoo.com>
2022-10-10 15:08:37 -0600

    Added some styling to the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,28 @@
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
 
-export default function Header({pageNames}) {
-    function ShowBooks() {
+export default function Header({ pageNames }) {
+  function ShowBooks() {
+    if (pageNames.length > 0) {
+      return pageNames.map((page, index) => (
+        <button className={classes.item} key={index}>
+          {page}
+        </button>
+      ));
     }
-    function ShowPages() {
-        console.log('this ran',pageNames ); //@DEBUG
-        if (pageNames.length > 0) {
-          return pageNames.map((page, index) => <p key={index}>{page}</p>)
-        }
-        if (pageNames.length < 0) {
-            return <div style={{color: 'white'}}>No Pages</div>
-        }
-      }
-    return (
-        <div className={classes.container}>
-          <ShowPages />
-        </div>
-    )
+    if (pageNames.length < 0) {
+      return <div style={{ color: "white" }}>No Pages</div>;
+    }
+  }
+
+  return (
+    <div className={classes.container}>
+      <h1 className={classes.logo}>My Notes</h1>
+      <div className={classes.itemContainer}>
+        <ShowBooks />
+        <button className={classes.item} >new +</button>
+        <input placeholder="search" />
+      </div>
+      <h3 className={classes.logOut}>Log Out</h3>
+    </div>
+  );
 }
