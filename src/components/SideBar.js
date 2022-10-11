b5c373f687f445dc7770888b188aac111018b7f8
Sean McGillis <slcidevice@yahoo.com>
2022-10-11 16:14:25 -0600

    Display grabber bar at edge of SideBar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,31 +1,59 @@
-import classes from './SideBar.module.css';
-import { useState } from 'react';
+import classes from "./SideBar.module.css";
+import { useState } from "react";
+
+function grabber() {
+    let grabArray = [];
+    let grabStyleTop = { margin: "0px" };
+    let grabStyleMiddle = { margin: "-6px" };
+    let grabStyleBottom = { margin: "0px" };
+    for (let i = 1; i < 5; i++) {
+      if (i === 1) {
+        grabArray.push(
+          <p key={i} style={grabStyleTop}>
+            {" "}
+            >{" "}
+          </p>
+        );
+      }
+      if (i === 4) {
+        grabArray.push(
+          <p key={i} style={grabStyleBottom}>
+            {" "}
+            >{" "}
+          </p>
+        );
+      }
+      if (i !== 4 && i !== 1) {
+        grabArray.push(
+          <p key={i} style={grabStyleMiddle}>
+            {" "}
+            >{" "}
+          </p>
+        );
+      }
+    }
+    return grabArray;
+  }
 
 export default function SideBar() {
-    const [isOpen, setIsOpen] = useState(false);
-    function grabber() {
-        let grabArray = []
-        let grabStyleTop = {margin: '0px'}
-        let grabStyleMiddle = {margin: '-6px'}
-        let grabStyleBottom = {margin: '0px'}
-        for (let i = 1; i < 5; i++) {
-            if (i === 1) {
-                grabArray.push(<p key={i} style={grabStyleTop}> > </p>)
-            }
-            if (i === 4) {
-                grabArray.push(<p key={i} style={grabStyleBottom}> > </p>)
-            }
-            if ((i !== 4) && (i !== 1)) {
-                grabArray.push(<p key={i} style={grabStyleMiddle}> > </p>)
-            }
-        }
-        return grabArray;
-    }
+  const [isOpen, setIsOpen] = useState(false);
 
-    console.log('grabber', grabber())
-
-
-    return <div className={`${classes.container} ${isOpen ? classes.open : classes.closed}`}>
-        <div onClick={() => setIsOpen(prevState => !prevState)} className={classes.grabber} >{grabber()}</div>
+  return (
+    <div
+      className={`${classes.container} ${
+        isOpen ? classes.open : classes.closed
+      }`}
+    >
+      <div
+        className={classes.grabberBar}
+      >
+        <div
+          onClick={() => setIsOpen((prevState) => !prevState)}
+          className={classes.grabber}
+        >
+          {grabber()}
+        </div>
+      </div>
     </div>
+  );
 }
