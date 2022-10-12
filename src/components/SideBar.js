965ee458b8973c65431fb2b5bbb2c2963326f930
Sean McGillis <slcidevice@yahoo.com>
2022-10-12 15:29:25 -0600

    SideBars open or close and reopen when selecting a new book or page

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import classes from "./SideBar.module.css";
 import Grabber from "./Grabber";
 
 export default function SideBar({itemName, selectedItemName, sideDisplayed}) {
   const [activePage, setActivePage] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    handleItemNameChange()
+  }, [itemName])
+
+  function handleItemNameChange() {
+    if(isOpen) {
+      setIsOpen(false);
+    } 
+    if (itemName && itemName.length > 0) {
+      setTimeout(() => {
+        setIsOpen(true);
+      }, '200')
+    }
+  }
+
 
   function handlePageSelection(page) {
     setActivePage(page);
