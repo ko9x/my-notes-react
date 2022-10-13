4713e37a51dadcf83d7ee5a15decb5842ca7112d
Sean McGillis <slcidevice@yahoo.com>
2022-10-13 17:12:02 -0600

    Added the side note and important note with some styling

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,15 +1,28 @@
-import classes from './Note.module.css';
+import classes from "./Note.module.css";
 
-export default function Note({selectedNotes}) {
-    console.log('selectedNotes from Note.js', selectedNotes); //@DEBUG
-    return (
-        
-        <div className={classes.container}>
-            {selectedNotes.length > 0 && selectedNotes.map((note) => (
-                <div key={note.id}><h1 style={{textAlign: 'start'}}>{note.title}</h1>
-                <p style={{textAlign: 'start'}} dangerouslySetInnerHTML={{__html: note.content}}></p></div>
-            )) }
-            
-        </div>
-    )
+export default function Note({ selectedNotes }) {
+  console.log("selectedNotes from Note.js", selectedNotes); //@DEBUG
+  return (
+    <div className={classes.container}>
+      {selectedNotes.length > 0 &&
+        selectedNotes.map((note) => (
+          <div className={classes.note} key={note.id}>
+            <h1>{note.title}</h1>
+            <p dangerouslySetInnerHTML={{ __html: note.content }}></p>
+            {note.important.length > 0 && (
+              <>
+                <h3 style={{color: 'red'}}>Important Note</h3>
+                <p dangerouslySetInnerHTML={{ __html: note.important }}></p>
+              </>
+            )}
+            {note.side.length > 0 && (
+              <>
+                <h3 style={{color: 'orange'}}>Side Note</h3>
+                <p dangerouslySetInnerHTML={{ __html: note.side }}></p>
+              </>
+            )}
+          </div>
+        ))}
+    </div>
+  );
 }
