4d982d70f4424fbdb7a1e3a33303d5811b177562
Sean McGillis <slcidevice@yahoo.com>
2022-10-14 13:30:29 -0600

    Formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
-import classes from './Footer.module.css';
+import classes from "./Footer.module.css";
 
 export default function Footer() {
-    return (
-        <div className={classes.container}>
-            <div className={classes.footerTextContainer}>
-                <p className={classes.footerText}>MyNotes 2022</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className={classes.container}>
+      <div className={classes.footerTextContainer}>
+        <p className={classes.footerText}>MyNotes 2022</p>
+      </div>
+    </div>
+  );
 }
