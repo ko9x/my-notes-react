03d2e5ab6b4024d4a72e3594944b1fd457358f99
Sean McGillis <slcidevice@yahoo.com>
2022-10-17 21:52:01 -0600

    Fixing a bug where the text did not slide with the SideBar when it opens and closes

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -8,6 +8,7 @@ export default function SideBar({
 }) {
   const [activeItem, setActiveItem] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
+  const [currentItems, setCurrentItems] = useState([]);
 
   useEffect(() => {
     handleItemNameArrayChange();
@@ -17,10 +18,19 @@ export default function SideBar({
     if (isOpen) {
       setIsOpen(false);
     }
-    if (itemNameArray && itemNameArray.length > 0) {
-      setTimeout(() => {
-        setIsOpen(true);
-      }, "200");
+    if (!isOpen && itemNameArray && itemNameArray.length > 0) {
+      setCurrentItems(itemNameArray);
+      setIsOpen(true);
+    }
+  }
+
+  // @TODO this is kind of clunky. We should have an ID or something to verify the array has changed
+  function handleTransitionEnd() {
+    if(itemNameArray[0] !== currentItems[0]) {
+      handleItemNameArrayChange();
+    }
+    if (itemNameArray.length !== currentItems.length) {
+      handleItemNameArrayChange();
     }
   }
 
@@ -31,13 +41,15 @@ export default function SideBar({
 
   return (
     <div
+      onTransitionEnd={e => {
+        handleTransitionEnd();
+      }}
       className={`${classes.container} ${
         isOpen ? classes.open : classes.closed
       } ${sideBarPosition === "left" ? classes.leftSideBarContainer : null}`}
     >
       <div className={classes.itemContainer}>
-        {itemNameArray && isOpen
-          ? itemNameArray.map((item, index) => {
+        {currentItems ? currentItems.map((item, index) => {
               return (
                 <button
                   onMouseDown={() => handleItemSelection(item)}
