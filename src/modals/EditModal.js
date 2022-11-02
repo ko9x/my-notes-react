17b922be4c337336f3ec086bde60735c634f81d3
Sean McGillis <slcidevice@yahoo.com>
2022-11-01 21:38:19 -0600

    Clicking the title of a text area changes the size of the text area

--- src/modals/EditModal.js
+++ src/modals/EditModal.js
@@ -1,5 +1,6 @@
 import Modal from "react-modal";
-import classes from './EditModal.module.css'
+import classes from "./EditModal.module.css";
+import { useState } from "react";
 
 const customStyles = {
   content: {
@@ -7,8 +8,8 @@ const customStyles = {
     width: "70vw",
     position: "relative",
     zIndex: "200",
-    marginLeft: '11vw',
-    marginTop: '8vh',
+    marginLeft: "11vw",
+    marginTop: "8vh",
     right: "auto",
     bottom: "auto",
     marginRight: "-50%",
@@ -24,6 +25,32 @@ export default function EditModal({
   pageList,
   sectionList,
 }) {
+  const [contentSize, setContentSize] = useState("medium");
+  const [sideSize, setSideSize] = useState(null);
+  const [importantSize, setImportantSize] = useState(null);
+
+  // @TODO I'm sure there is a better way to refactor this
+  function toggleContentSize() {
+    if (contentSize === "large") {
+      setContentSize("medium");
+    } else {
+      setContentSize("large");
+    }
+  }
+  function toggleSideSize() {
+    if (sideSize === null) {
+      setSideSize("medium");
+    } else {
+      setSideSize(null);
+    }
+  }
+  function toggleImportantSize() {
+    if (importantSize === null) {
+      setImportantSize("medium");
+    } else {
+      setImportantSize(null);
+    }
+  }
   return (
     <Modal
       isOpen={isModalOpen}
@@ -59,24 +86,42 @@ export default function EditModal({
               value={noteToEdit?.title}
               className={classes.modalInput}
             />
-            <h2>Content</h2>
-            <textarea className={classes.modalTextArea}>{noteToEdit?.content}</textarea>
-            <h3>Side Note</h3>
-            <input
-              type="text"
-              value={noteToEdit?.side ? noteToEdit.side : null}
-              className={classes.modalInput}
-            />
-            <h3>Important Note</h3>
-            <input
-              type="text"
-              value={noteToEdit?.important ? noteToEdit.important : null}
-              className={classes.modalInput}
-            />
+            <h2 onClick={() => toggleContentSize()}>Content</h2>
+            <textarea
+              className={`${classes.modalTextArea} ${
+                contentSize === "large"
+                  ? classes.modalTextAreaLargeHeight
+                  : classes.modalTextAreaMediumHeight
+              }`}
+            >
+              {noteToEdit?.content}
+            </textarea>
+            <h3 onClick={() => toggleSideSize()}>Side Note</h3>
+            <textarea
+              placeholder={noteToEdit?.side ? null : 'Add a side note'}
+              className={`${classes.modalTextArea} ${
+                sideSize === "medium" ? classes.modalTextAreaMediumHeight : null
+              }`}
+            >
+              {noteToEdit?.side}
+            </textarea>
+            <h3 onClick={() => toggleImportantSize()}>Important Note</h3>
+            <textarea
+              placeholder={noteToEdit?.side ? null : 'Add an important note'}
+              className={`${classes.modalTextArea} ${
+                importantSize === "medium"
+                  ? classes.modalTextAreaMediumHeight
+                  : null
+              }`}
+            >
+              {noteToEdit?.important}
+            </textarea>
           </form>
         </div>
         <div className={classes.closeButtonContainer}>
-          <button className={classes.closeButton} onClick={closeModal}>close</button>
+          <button className={classes.closeButton} onClick={closeModal}>
+            close
+          </button>
         </div>
       </div>
     </Modal>
