a6a7f11ebc80de787284a90678f29e94f47ef98a
Sean McGillis <slcidevice@yahoo.com>
2023-01-23 15:02:55 -0700

    The radio inputs are now actionButton color

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -1,3 +1,5 @@
+import './Radio.module.css';
+
 export default function Radio({ nameArray, selectionFunction, selectedItem }) {
   return (
     <div
@@ -10,7 +12,7 @@ export default function Radio({ nameArray, selectionFunction, selectedItem }) {
       {nameArray?.length > 0 &&
         nameArray.map((itemName, index) => (
           <div key={`${itemName}${index}`}>
-            <input onChange={(e) => selectionFunction(e)} type="radio" id={itemName} checked={itemName === selectedItem ? true : false} /> <label>{itemName}</label>
+            <input onChange={(e) => selectionFunction(e)} type="radio" id={itemName} checked={itemName === selectedItem ? true : false} /><label>{itemName}</label>
           </div>
         ))}
     </div>
