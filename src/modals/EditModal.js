636d061a4a85c2d06e2543b3545f8d83e063f837
Sean McGillis <slcidevice@yahoo.com>
2023-02-01 15:24:58 -0700

    Moved some functions into the HelperFunctions.js

--- src/modals/EditModal.js
+++ src/modals/EditModal.js
@@ -5,6 +5,7 @@ import { useEffect, useRef } from "react";
 import HelperButtons from "../components/HelperButtons";
 import RadioManager from "../components/RadioManager";
 import { ref, child, push, update } from "firebase/database";
+import { insertHelperText, helpers, fixCaret } from "../helpers/HelperFunctions";
 
 const customStyles = {
   content: {
@@ -20,56 +21,6 @@ const customStyles = {
     backgroundColor: "grey",
   },
 };
-
-const helpers = {
-  codeWrap: {id: 1, name: 'code-wrap', code: '<pre><code></code></pre>'},
-  pTag: {id: 2, name: 'p-tag', code: '<p></p>'},
-  aTag: {id: 3, name: 'a-tag', code: '<a href="" target="_blank"><a/>'},
-  lineBreak: {id: 4, name: 'line-break', code: '<br>'},
-  nonBreakingSpace: {id: 5, name: 'non-breaking-space', code: ' &nbsp '},
-  listItem: {id: 6, name: 'list-item', code: '<li></li>'},
-}
-
-function setCursorPosition(cursorPosition, selectedRef, helper) {
-  if(helper === helpers.aTag) {
-    return selectedRef.current.selectionEnd = cursorPosition + 9;
-  }
-  if(helper === helpers.pTag) {
-    return selectedRef.current.selectionEnd = cursorPosition + 3;
-  }
-  if(helper === helpers.codeWrap) {
-    return selectedRef.current.selectionEnd = cursorPosition + 11;
-  }
-  if(helper === helpers.nonBreakingSpace) {
-    return selectedRef.current.selectionEnd = cursorPosition + 7;
-  }
-  if(helper === helpers.listItem || helpers.lineBreak) {
-    return selectedRef.current.selectionEnd = cursorPosition + 4;
-  }
-}
-
-function insertHelperText(selectedRef, helper) {
-  let currentValue = selectedRef.current.value;
-  let cursorPosition = selectedRef.current.selectionStart;
-  let valueBeforeCursor = selectedRef.current.value.substring(0, cursorPosition);
-  let valueAfterCursor = selectedRef.current.value.substring(cursorPosition, currentValue.length);
-  selectedRef.current.value = valueBeforeCursor + helper.code + valueAfterCursor;
-  selectedRef.current.focus();
-  setCursorPosition(cursorPosition, selectedRef, helper);
-}
-
-function fixCaret(noteText) {
-  let arr = noteText.split('<pre><code>').map(el => el.split('</code></pre>')).reduce((acc, curr) => acc.concat(curr))
-  let newArr = arr.map((section, index) => {
-    if(index % 2 !== 0) {
-      let newPart = section.split('<').join('&#60');
-      return `<pre><code>${newPart}</code></pre>`;
-    } else {
-      return section;
-    }
-  });
-  return newArr.join('');
-}
 
 export default function EditModal({
   isEditModalOpen,
@@ -190,19 +141,6 @@ export default function EditModal({
   function handleCloseModal() {
     resetModal();
     closeModal();
-  }
-
-  function locationMoved(updatedNote) {
-    if (noteToEdit.book !== updatedNote.book) {
-      return true;
-    }
-    if (noteToEdit.page !== updatedNote.page) {
-      return true;
-    }
-    if (noteToEdit.section !== updatedNote.section) {
-      return true;
-    }
-    return false;
   }
 
   function missingRadioValue() {
